aa949817d66f5b781371844548075f7fcacb82b1
Derek Benak <benak.derek@gmail.com>
2018-10-26 16:53:27 -0700

    Completed second form

---
+++ src/components/Secret.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="component">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        message: 'Always use Inspect on your web apps'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .component {
+    background: #e8e8e8;
+    min-height: 400px;
+  }
+</style>
