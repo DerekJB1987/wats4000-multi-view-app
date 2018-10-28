6f7edbaf35d1a8c781280b94dc32d9f656c00a46
Derek Benak <benak.derek@gmail.com>
2018-10-27 18:06:45 -0700

    debugging project

--- src/components/Secret.vue
+++ src/components/Secret.vue
@@ -8,7 +8,7 @@
   export default {
     data () {
       return {
-        message: 'Always use Inspect on your web apps'
+        message: 'Always use your Developer debugging tools with your web apps'
       }
     }
   }
